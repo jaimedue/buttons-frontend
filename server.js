44793878573079dbf4b4114b4dd8a50896a65ddc
jaimedue <jdue930@gmail.com>
2022-06-07 10:17:38 -0500

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
+const publicPath = path.join(__dirname, '..', 'public')
+
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', (req, res) => {
