fec768a1793c07d19b4c1e34f168a87572d5d0eb
jaimedue <jdue930@gmail.com>
2022-06-07 10:11:56 -0500

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require("http-proxy-middleware")
 
 module.exports = app => {
-    app.use(proxy("/api/*", { target: "https://buttons-backend.herokuapp.com/"}))
+    app.use(proxy("/*", { target: "https://buttons-backend.herokuapp.com/"}))
 }
