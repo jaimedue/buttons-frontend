1b9d20ba13a6d8312bfc2d0a4b5787e7d9d1eb18
jaimedue <jdue930@gmail.com>
2022-06-07 10:25:57 -0500

    server

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-const publicPath = path.join(__dirname, 'public')
+// const publicPath = path.join(__dirname,'..', 'public')
 
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'))
+    res.sendFile(path.join('public/index.html'))
 })
 
 app.listen(port, () => console.log(`App is live on port ${port}`))
